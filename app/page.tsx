73b16291b17f8d45b4cf8cc158edff8ac7431ea1
Nourhan392003 <https://github.com/Nourhan392003>
2026-03-08 13:20:50 +0200

    fix: remove stray text in services

--- app/page.tsx
+++ app/page.tsx
@@ -666,7 +666,7 @@ ${
 
 
 
-            )
+            
 
             <h3 className="text-xl font-bold text-green-400 mb-3">
               {service.title}
@@ -727,9 +727,7 @@ ${
     </button>
   </div>
 )}
-
-
-                  </div>
+       </div>
                 ))}
               </div>
             </div>
